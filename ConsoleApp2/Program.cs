fb695474717a35ff2bc09c84242338b4bcd9e43b
Fabio Maschi <fabio.maschi@u-psud.fr>
2018-02-23 14:20:28 +0100

    FSUIPC Handler

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace ConnectToProfilerSDK
+namespace FAQU
 {
     using System;
     using System.Linq;
@@ -12,12 +12,7 @@ namespace ConnectToProfilerSDK
     class Program
     {
         static Fsuipc fsuipc = new Fsuipc();    // Our main fsuipc object!
-        static EventClient eventClient;
-        static bool result = false;            // Return boolean for FSUIPC method calls
-        static int dwResult = -1;              // Variable to hold returned results
-        static int dwFSReq = 0;             // Any version of FS is OK
-        static int token = -1;
-        static float FobT = .0f;
+        static EventClient eventClient;         // Skalarki HW object
 
         private static IPAddress GetLocalIPAddress()
         {
@@ -44,112 +39,6 @@ namespace ConnectToProfilerSDK
                 Console.ReadLine();
                 eventClient.Disconnect();
             }
-
-            /*
-            using (eventClient = new EventClient(localIP, 53000, (e, s) => { OnHardwareEvent(e, s); }, null))
-            {
-                eventClient.ConnectionStateChanged += (s, a) =>
-                {
-                    Console.WriteLine("Client is now {0}", a.Connected ? "connected" : "not connected");
-                };
-
-                eventClient.ConnectedDevicesChanged += (s, a) =>
-                {
-                    Console.WriteLine("Hardware {0} has {1}", a.Device, a.Connected ? "connected" : "disconnected");
-                };
-
-                Console.WriteLine("Press <ENTER> to connect to the SkalarkiIO Profiler");
-                Console.ReadLine();
-
-                eventClient.ConnectAsync().Wait();
-
-                Console.WriteLine("Press <ENTER> to register an event");
-                Console.ReadLine();
-
-                // Register an event
-                //var totalEvents = Switches.GLARE.All.Concat(Encoders.GLARE.All);
-                var totalEvents = Switches.OVHD.All.Concat(Encoders.OVHD.All);
-
-                eventClient.RegisterEvents(Switches.OVHD.All.Concat(Encoders.OVHD.All));
-
-                Console.WriteLine("Press <ENTER> to set display text");
-                Console.ReadLine();
-
-
-                Console.WriteLine("Press <ENTER> to connect FSUIPC");
-                Console.ReadLine();
-
-                fsuipc.FSUIPC_Initialization();
-                result = fsuipc.FSUIPC_Open(dwFSReq, ref dwResult); //Ouverture de la connexion avec FSUIPC
-                if (result)
-                {
-                    Console.WriteLine("FSUIPC initialized");
-                }
-                else
-                    Console.WriteLine("FSUIPC NOT initialized");
-
-                Console.WriteLine("Press <ENTER> to read Fuel");
-                Console.ReadLine();
-
-                byte[] buffer = new byte[255];
-
-                result = fsuipc.FSUIPC_Read(0x30C8, 8, ref token, ref dwResult); //Lecture de la masse de l'avion
-                result = fsuipc.FSUIPC_Process(ref dwResult);
-
-                result = fsuipc.FSUIPC_Get(ref token, 8, ref buffer);
-
-                double loaded = BitConverter.ToDouble(buffer, 0); //Conversion du tableau de bytes en double
-
-                loaded = (loaded * 32.174049); //Conversion de slug en lbs
-                Console.WriteLine("Current mass in lbs: " + loaded);
-
-                result = fsuipc.FSUIPC_Read(0x3BFC, 4, ref token, ref dwResult); //Lecture du Zero Fuel Weight (masse de l'avion + chargement - fuel)
-                result = fsuipc.FSUIPC_Process(ref dwResult);
-                result = fsuipc.FSUIPC_Get(ref token, ref dwResult);
-                float zfw = dwResult / 256; //Conversion demandée par FSUIPC
-                Console.WriteLine("ZFW in lbs: " + zfw);
-
-                dwResult = -1;
-                token = -1;
-
-                float fob = (float)loaded - zfw; //Calcul du fuel on board (FOB)
-
-                float fobKg = fob * 0.45359237f; //Conversion du FOB en KG
-
-                Console.WriteLine("FOB in lbs: " + fob);
-                Console.WriteLine("FOB in kg: " + fobKg);
-
-                FobT = fobKg / 1000;
-
-                //float fob = (float)mass - zfw;
-
-                if (result)
-                {
-                    Console.WriteLine("Fuel on board : " + fob);
-                }
-                else
-                if (!result)
-                    Console.WriteLine("Read error");
-
-                fsuipc.FSUIPC_Close();
-
-                Console.WriteLine("FSUIPC closed");
-
-                Console.WriteLine("Press <ENTER> to reset display text");
-                Console.ReadLine();
-
-                eventClient.SetDisplayText(Displays.OVHD.REFUEL.PRESELECTED, "---");
-                eventClient.SetDisplayText(Displays.OVHD.REFUEL.PRESELECTED, "---");
-
-
-                Console.WriteLine("Press <ENTER> to disconnect from the SkalarkiIO Profiler");
-                Console.ReadLine();
-
-                eventClient.Disconnect();
-            }
-        }*/
-
-
         }
     }
 
@@ -163,6 +52,7 @@ namespace ConnectToProfilerSDK
         private State currentState;
         private EventClient hardwareClient;
         private Fsuipc fsuipcClient;
+        private FSUIPCHandler fsuipcHandler;
         private Timer timer;
 
         private float preselectedFuel;
@@ -182,9 +72,7 @@ namespace ConnectToProfilerSDK
         {
             this.hardwareClient = skarlaki;
             this.fsuipcClient = fsuipc;
-
-
-            
+            this.fsuipcHandler = new FSUIPCHandler(this.fsuipcClient);
 
             this.hardwareClient.ConnectionStateChanged += (s, a) =>
             {
@@ -206,6 +94,7 @@ namespace ConnectToProfilerSDK
                         }
                         else
                         {
+                            FSUIPCHandler.PrintErrorCode(dwResult);
                             SetNextState(State.Fault);
                         }
                     }
@@ -430,6 +319,9 @@ namespace ConnectToProfilerSDK
 
         private void DoRefuel()
         {
+            UpdateFSUIPC();
+            // TODO: update this.actualFuel by calling GetTotalFuelLevel();
+
             if (this.preselectedFuel > this.actualFuel)
             {
                 this.actualFuel += FUEL_STEP;
@@ -443,7 +335,6 @@ namespace ConnectToProfilerSDK
                 timer.Dispose();
                 SetNextState(State.Finished);
             }
-            UpdateFSUIPC();
             UpdateLCD();
         }
 
@@ -471,9 +362,6 @@ namespace ConnectToProfilerSDK
             float zfw = dwResult / 256; //Conversion demandée par FSUIPC
             Console.WriteLine("ZFW in lbs: " + zfw);
 
-            dwResult = -1;
-            token = -1;
-
             float fob = (float)loaded - zfw; //Calcul du fuel on board (FOB)
 
             float fobKg = fob * 0.45359237f; //Conversion du FOB en KG
@@ -483,6 +371,9 @@ namespace ConnectToProfilerSDK
 
         private float GetMaxFuelCapacity()
         {
+            //return FSUIPCHandler.GetTotalFuelCapacity(this.fsuipcClient);
+
+
             bool result = false;            // Return boolean for FSUIPC method calls
             int dwResult = -1;              // Variable to hold returned results
             int token = -1;
@@ -514,13 +405,325 @@ namespace ConnectToProfilerSDK
             Console.WriteLine("max capacity en Kg : " + (fuelweightKgL * capacityL));
 
             return capacityL;
-
         }
 
         private void UpdateFSUIPC()
         {
             // TODO send to FSUIPC the actual value
             // TODO if no connection --> stete = FAULT and (timer.dispose()!)
+
+            float[] levels = FSUIPCHandler.GetTanksCurrentLevel(fsuipcClient);
+            float[] capacities = FSUIPCHandler.GetTanksCapacity(fsuipcClient);
+
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            //  0       1           2           3           4           5           6
+            int[] order = {0, 1, 4, 2, 5, 3, 6};
+            foreach(int idx in order)
+            {
+                if(IsPossibleToRefuel(FUEL_STEP, ref levels[i], capacities[i]))
+                {
+                    SetNewTankLevel(fsuipcClient, i, levels[i]);
+                    break;
+                }
+            }
+        }
+
+        private bool IsPossibleToRefuel(float step, float level, float capacity)
+        {
+            float current = capacity * levels; // TODO maybe it's levels/100
+            if (levels != 1)                   // TODO either 1 or 100 ?
+            {
+                current = current + step;
+                if (current > capacity)
+                {
+                    current = capacity;
+                }
+                level = current / capacity;    // TODO * 100 ?
+                return true;
+            }
+            return false;
+        }
+
+        public void TestToCarryOut()
+        {
+            // Comparing the two methods
+
+            float[] capacities = fsuipcHandler.GetTanksCapacity(this.fsuipcClient);
+            float[] levels = fsuipcHandler.GetTanksCurrentLevel(this.fsuipcClient);
+
+            float current = 0;
+            float capacity = 0;
+            for(int i=0; i<7; i++)
+            {
+                current = current + (capacities[i] * levels[i]); // TODO: maybe it's level/100
+                capacity = capacity + capacities[i];
+            }
+
+            float quentinActual = GetActualFuel();
+            float quentinMax    = GetMaxFuelCapacity();
+
+            Console.WriteLine(">> Comparing the methods")
+            Console.WriteLine(" float GetActualFuel() : {0} in Kg x 1000", quentinActual);
+            Console.WriteLine(" float[] Individually  : {0} in Kg x 1000", total;
+            Console.WriteLine(".");
+            Console.WriteLine(" float GetMaxFuelCapacity() : {0} in liters?", quentinMax);
+            Console.WriteLine(" float GetTanksCapacity()   : {0} in Kg x 1000", capacity);
+        }
+    }
+
+    class FSUIPCHandler
+    {
+        public fsuipc fsuipcClient;
+        private bool isConnected;
+
+        //
+        private bool result;         // Return boolean for FSUIPC method calls
+        private int dwResult = -1;   // Variable to hold returned results
+        private int token = -1;
+        private byte[] buffer = new byte[255];
+        //
+        private static readonly Int32[] AddrTabTotalFuelCapacity = new Int32[7] { 0x0B78, 0x0B80, 0x0B88, 0x0B90, 0x0B98, 0x0BA0, 0x0BA8 }; // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+
+        private static readonly Int32[] AddrTabCurrentFuelLevels = new Int32[7] { 0x0B74, 0x0B7C, 0x0B84, 0x0B8C, 0x0B94, 0x0B9C, 0x0BA4 }; // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+
+        // TODO: add exceptions for results = false
+        // TODO: encapsulate fsuipcClient here
+
+        public FSUIPCHandler(fsuipc fsuipcClient)
+        {
+            this.fsuipcClient = fsuipcClient;
+            this.isConnected = false;
+        }
+
+        public bool IsConnected
+        {
+            public get { return this.isConnected; }
+            private set { }
+        }
+
+        public bool Connect()
+        {
+            // TODO
+        }
+
+        public float[] GetTanksCapacity()
+        {
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            // Unit: Kg x 1000
+
+            float[] tanks = new float[7];
+            int i = 0;
+            float fuelDensity; // pounds per US gallon
+
+            // Fuel weight as pounds per gallon
+            result = this.fsuipcClient.FSUIPC_Read(0x0AF4, 4, ref token, ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+            if(result)
+                fuelDensity = dwResult / 256.0f;
+            else
+            {
+                fuelDensity = 0;
+                PrintErrorCode(dwResult);
+            }
+
+            // Tanks capacity in US Gallons
+            foreach (Int32 adr in AddrTabTotalFuelCapacity)
+            {
+                result = this.fsuipcClient.FSUIPC_Read(adr, 4, ref token, ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+                if(result)
+                {
+                    // Convert from US Gallons to Pounds then to Kg then to 1000xKg
+                    // 1000 x Kg = [gal] * [lbs/gal] * [kg/lbs] / []
+                    tanks[i] = dwResult * fuelDensity * 0.453592f / 1000.0f;
+                }
+                else
+                {
+                    tanks[i] = 0;
+                    PrintErrorCode(dwResult);
+                }
+            }
+
+            return tanks;
+        }
+
+        public float[] GetTanksCurrentLevel()
+        {
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            // Unit: percentage
+
+            float[] tanks = new float[7];
+            int i = 0;
+
+            foreach (Int32 adr in AddrTabCurrentFuelLevels)
+            {
+                result = this.fsuipcClient.FSUIPC_Read(adr, 4, ref token, ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+                if(result)
+                {
+                    tanks[i] = dwResult / 128.0f / 65536.0f;
+                }
+                else
+                {
+                    tanks[i] = 0;
+                    PrintErrorCode(dwResult);
+                }
+            }
+
+            return tanks;
+        }
+
+
+        public float GetTotalFuelCapacity()
+        {
+            // Unit: Kg x 1000
+
+            float[] capacities = GetTanksCapacity();
+            float total = 0;
+
+            foreach (float capacity in capacities)
+                total = total + capacity;
+
+            return total;
+        }
+
+        public float GetTotalFuelLevel()
+        {
+            float[] capacities = FSUIPCHandler.GetTanksCapacity();
+            float[] levels = FSUIPCHandler.GetTanksCurrentLevel();
+
+            float current = 0;
+            float capacity = 0;
+            for(int i=0; i<7; i++)
+                current = current + (capacities[i] * levels[i]);
+
+            return current;
+        }
+
+        public double GetCurrentLoadedWeight()
+        {
+            // Unit: lbs
+            double loaded;
+
+            // Current loaded weight in lbs.
+            result = this.fsuipcClient.FSUIPC_Read(0x30C0, 8, ref token, ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Get(ref token, 8, ref buffer);
+
+            if (result)
+            {
+                // Conversion du tableau de bytes en double (Float64)
+                loaded = BitConverter.ToDouble(buffer, 0);
+            }
+            else
+            {
+                loaded = 0;
+                PrintErrorCode(dwResult);
+            }
+
+            return loaded;
+        }
+
+        public void SetNewTankLevel(int tank_id, float level)
+        {
+            level = level * 128 * 65536;
+            result = this.fsuipcClient.FSUIPC_Write(AddrTabCurrentFuelLevels[tank_id], level, token, dwResult);
+            result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+
+            if(!result)
+                PrintErrorCode(dwResult);
+        }
+
+
+        public static void PrintErrorCode(int code)
+        {
+            Console.WriteLine(translateResultCode(code));
+        }
+
+        public static string translateResultCode(int code) 
+        {
+            switch (code) 
+            {
+
+                case Fsuipc.FSUIPC_ERR_OK:
+                    return code + " = FSUIPC_ERR_OK";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_OPEN:
+                    return code + " = FSUIPC_ERR_OPEN";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_NOFS:
+                    return code + " = FSUIPC_ERR_NOFS";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_REGMSG:
+                    return code + " = FSUIPC_ERR_REGMSG";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_ATOM:
+                    return code + " = FSUIPC_ERR_ATOM";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_MAP:
+                    return code + " = FSUIPC_ERR_MAP";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_VIEW:
+                    return code + " = FSUIPC_ERR_VIEW";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_VERSION:
+                    return code + " = FSUIPC_ERR_VERSION";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_WRONGFS:
+                    return code + " = FSUIPC_ERR_WRONGFS";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_NOTOPEN:
+                    return code + " = FSUIPC_ERR_NOTOPEN";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_NODATA:
+                    return code + " = FSUIPC_ERR_NODATA";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_TIMEOUT:
+                    return code + " = FSUIPC_ERR_TIMEOUT";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_SENDMSG:
+                    return code + " = FSUIPC_ERR_SENDMSG";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_DATA:
+                    return code + " = FSUIPC_ERR_DATA";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_RUNNING:
+                    return code + " = FSUIPC_ERR_RUNNING";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_SIZE:
+                    return code + " = FSUIPC_ERR_SIZE";
+                    break;
+
+                case Fsuipc.FSUIPC_ERR_BUFOVERFLOW:
+                    return code + " = FSUIPC_ERR_BUFOVERFLOW";
+                    break;
+
+                default:
+                    return code.ToString();
+                    break;
+
+            }
         }
     }
 }
@@ -553,3 +756,5 @@ namespace ConnectToProfilerSDK
             break;
     }
 }*/
+
+// TODO play a song during refuelling
