4baa4bfad2d2b5b0173d408191804166a7e1f57d
Fabio Maschi <fabio.maschi@u-psud.fr>
2018-03-06 11:01:49 +0100

    Classes organisation

---
+++ ConsoleApp2/FAQUBrickRefuelling.cs
@@ -0,0 +1,362 @@
+
+namespace FAQU
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using SkalarkiIO.SDK;
+    using System.Timers;
+    using FsuipcSdk;
+    using System.Collections.Generic;
+
+    // To use this class : link OnHardwareEvent(), provide EventClient and Fsuipc objects through Setup();
+    class FAQUBrickRefuelling
+    {
+        private static float FUEL_SLCT_STEP = 0.10f;    // [Kg x 1000]
+        private static float FUEL_LOAD_STEP = 0.01f;    // [Kg x 1000]
+        private static int TIMER_INTERVAL = 250;        // [ms]
+
+        private enum State { Offline, Off, Selection, Refuelling, Finished, Fault };
+        // Offline    : No Skalarki (hardware) connection
+        // Off        : Refuelling off
+        // Selection  : Choosing fuel quantity
+        // Refuelling : well... refuelling
+        // Finished   : I've chosen the best words to describe the states
+        // Fault      : Important! Skalarki is connected, but no FSUIPC (software) connection
+
+        private State currentState;
+        private EventClient hardwareClient;
+        private FSUIPCHandler fsuipcHandler;
+        private Fsuipc fsuipcClient = new Fsuipc();
+        private Timer timer;
+
+        private float preselectedFuel;
+        private float actualFuel;
+        private float maxFuelCapacity;
+        //
+        private float[] tanksCapacity;
+        private static int[] order = {
+            FSUIPCHandler.TANK_LEFT_TIP_ID,
+            FSUIPCHandler.TANK_RIGHT_TIP_ID,
+
+            FSUIPCHandler.TANK_LEFT_MAIN_ID,
+            FSUIPCHandler.TANK_RIGHT_MAIN_ID,
+
+            FSUIPCHandler.TANK_LEFT_AUX_ID,
+            FSUIPCHandler.TANK_RIGHT_AUX_ID,
+
+            FSUIPCHandler.TANK_CENTRE_ID
+            };
+
+        public FAQUBrickRefuelling()
+        {
+            SetNextState(State.Offline);
+            this.preselectedFuel = 0;
+            this.maxFuelCapacity = 0;
+            this.actualFuel = 0;
+        }
+
+
+        public void Setup(EventClient skarlaki, FSUIPCHandler fsuipc)
+        {
+            this.hardwareClient = skarlaki;
+            this.fsuipcHandler = fsuipc;
+
+            this.hardwareClient.ConnectionStateChanged += (s, skalarki) =>
+            {
+                if (skalarki.Connected)
+                    ConnectToFSUIPC();
+                else
+                    SetNextState(State.Offline);
+            };
+
+            try
+            {
+                hardwareClient.ConnectAsync().Wait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Skalarki.ConnectAsync(): " + e.Message);
+                SetNextState(State.Offline);
+            }
+        }
+
+        private void ConnectToFSUIPC()
+        {
+            bool result = false;
+            if(!this.fsuipcHandler.IsConnected)
+            {
+                result = this.fsuipcHandler.Connect();
+                if(result)
+                {
+                    // TestToCarryOut();
+                    this.maxFuelCapacity = fsuipcHandler.GetTotalFuelCapacity();
+                    this.hardwareClient.RegisterEvents(Switches.OVHD.All.Concat(Encoders.OVHD.All));
+                    SetNextState(State.Off);
+                }
+                else
+                    SetNextState(State.Fault);
+                UpdateLCD();
+            }
+        }
+
+        public void OnHardwareEvent(IOEvent hardwareEvent, object state)
+        {
+            // Filter (Group = Refuel) and (Source = Switch) and (Event = True)
+            if ((hardwareEvent.Group == Group.REFUEL) && (hardwareEvent.Source == HardwareSource.Switch) && (hardwareEvent.ValueAsBool() == true))
+            {
+                switch (currentState)
+                {
+                    case State.Off:
+                        {
+                            if (hardwareEvent.Event == Event.POWER)
+                            {
+                                SetNextState(State.Selection);
+                            }
+                            break;
+                        }
+                    case State.Selection:
+                        {
+                            switch (hardwareEvent.Event)
+                            {
+                                case Event.POWER:
+                                    {
+                                        SetNextState(State.Off);
+                                        break;
+                                    }
+                                case Event.INCREASE:
+                                    {
+                                        IncreasePreselectedFuel();
+                                        break;
+                                    }
+                                case Event.DECREASE:
+                                    {
+                                        DecreasePreselectedFuel();
+                                        break;
+                                    }
+                                case Event.REFUELING:
+                                    {
+                                        SetNextState(State.Refuelling);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case State.Refuelling:
+                        {
+                            switch (hardwareEvent.Event)
+                            {
+                                case Event.POWER:
+                                    {
+                                        timer.Dispose();
+                                        SetNextState(State.Off);
+                                        break;
+                                    }
+                                case Event.REFUELING:
+                                    {
+                                        timer.Dispose();
+                                        SetNextState(State.Selection);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case State.Finished:
+                        {
+                            switch (hardwareEvent.Event)
+                            {
+                                case Event.POWER:
+                                    {
+                                        SetNextState(State.Off);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case State.Fault:
+                        {
+                            switch (hardwareEvent.Event)
+                            {
+                                case Event.POWER:
+                                    {
+                                        ConnectToFSUIPC();
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                }
+                UpdateLCD();
+            }
+        }
+
+        private void UpdateLCD()
+        {
+            switch (currentState)
+            {
+                case State.Off:
+                    {
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.ACTUAL, "   ");
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.PRESELECTED, "   ");
+                        break;
+                    }
+                case State.Selection:
+                case State.Refuelling:
+                case State.Finished:
+                    {
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.ACTUAL, this.actualFuel.ToString());
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.PRESELECTED, this.preselectedFuel.ToString());
+                        break;
+                    }
+                case State.Fault:
+                    {
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.ACTUAL, "---");
+                        hardwareClient.SetDisplayText(Displays.OVHD.REFUEL.PRESELECTED, "---");
+                        break;
+                    }
+            }
+        }
+
+        private void SetNextState(State nextState)
+        {
+            Console.WriteLine("Next state : " + nextState);
+            this.currentState = nextState;
+
+            switch (currentState)
+            {
+                case State.Off:
+                    {
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.END }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.CKPT }, false);
+                        break;
+                    }
+                case State.Selection:
+                    {
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRON }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.END }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.CKPT }, true);
+                        this.preselectedFuel = fsuipcHandler.GetTotalFuelLevel();
+                        this.actualFuel = fsuipcHandler.GetTotalFuelLevel();
+                        break;
+                    }
+                case State.Refuelling:
+                    {
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRON }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGON }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.END }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.CKPT }, true);
+                        StartRefuel();
+                        break;
+                    }
+                case State.Finished:
+                    {
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRON }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.END }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.CKPT }, true);
+                        break;
+                    }
+                case State.Fault:
+                    {
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.PWRFAULT }, true);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGON }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.REFUELINGFAULT }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.END }, false);
+                        hardwareClient.SetOutputs(new[] { Outputs.OVHD.REFUEL.CKPT }, false);
+                        break;
+                    }
+            }
+        }
+
+        private void IncreasePreselectedFuel()
+        {
+            this.preselectedFuel += FUEL_SLCT_STEP;
+            if (this.preselectedFuel > maxFuelCapacity)
+                this.preselectedFuel = maxFuelCapacity;
+        }
+
+        private void DecreasePreselectedFuel()
+        {
+            this.preselectedFuel -= FUEL_SLCT_STEP;
+            if (this.preselectedFuel < 0)
+                this.preselectedFuel = 0;
+        }
+
+        private void StartRefuel()
+        {
+            this.tanksCapacity = fsuipcHandler.GetTanksCapacity();
+
+            timer = new Timer();
+            timer.Interval = TIMER_INTERVAL;
+
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += (sender, args) =>
+            {
+                DoRefuel();
+            };
+
+            // Have the timer fire repeated events (true is the default)
+            timer.AutoReset = true;
+
+            // Start the timer
+            timer.Enabled = true;
+        }
+
+        private void DoRefuel()
+        {
+            float[] levels = fsuipcHandler.GetTanksCurrentLevel();
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            //  0       1           2           3           4           5           6
+            float step = (this.preselectedFuel > this.actualFuel) ? FUEL_LOAD_STEP : -FUEL_LOAD_STEP;
+
+            foreach(int idx in order)
+            {
+                if (IsPossibleToRefuel(step, ref levels[idx], tanksCapacity[idx]))
+                {
+                    fsuipcHandler.SetNewTankLevel(idx, levels[idx]);
+                    break;
+                }
+            }
+
+            this.actualFuel = fsuipcHandler.GetTotalFuelLevel();
+            if ((this.actualFuel >= this.preselectedFuel) && (this.actualFuel <= this.preselectedFuel + FUEL_LOAD_STEP))
+            {
+                timer.Dispose();
+                SetNextState(State.Finished);
+            }
+
+            UpdateLCD();
+        }
+
+        private bool IsPossibleToRefuel(float step, ref float level, float capacity)
+        {
+            //Console.WriteLine("Check level={0} cap¨={1}", level, capacity);
+            float current = capacity * level;
+            if (level != 1 && capacity != 0)
+            {
+                current = current + step;
+                if (current > capacity)
+                {
+                    current = capacity;
+                }
+                level = current / capacity;
+                //Console.WriteLine("  OUIIIIII¨new level={0}", level);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp2/FSUIPCHandler.cs
@@ -0,0 +1,267 @@
+
+namespace FAQU
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using SkalarkiIO.SDK;
+    using System.Timers;
+    using FsuipcSdk;
+    using System.Collections.Generic;
+
+    class FSUIPCHandler
+    {
+        public Fsuipc fsuipcClient;
+        private bool isConnected;
+        //
+        private bool result;         // Return boolean for FSUIPC method calls
+        private int dwResult = -1;   // Variable to hold returned results
+        private int token = -1;
+        private byte[] buffer = new byte[255];
+        //
+        // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+        private static readonly Int32[] AddrTabTotalFuelCapacity = new Int32[7] { 0x0B78, 0x0B80, 0x0B88, 0x0B90, 0x0B98, 0x0BA0, 0x0BA8 };
+        public static readonly Int32[] AddrTabCurrentFuelLevels = new Int32[7] { 0x0B74, 0x0B7C, 0x0B84, 0x0B8C, 0x0B94, 0x0B9C, 0x0BA4 };
+        public static readonly int TANK_CENTRE_ID = 0;
+        public static readonly int TANK_LEFT_MAIN_ID = 1;
+        public static readonly int TANK_LEFT_AUX_ID = 2;
+        public static readonly int TANK_LEFT_TIP_ID = 3;
+        public static readonly int TANK_RIGHT_MAIN_ID = 4;
+        public static readonly int TANK_RIGHT_AUX_ID = 5;
+        public static readonly int TANK_RIGHT_TIP_ID = 6;
+        //
+        // TODO: add exceptions for results = false
+
+        public FSUIPCHandler()
+        {
+            this.fsuipcClient = new Fsuipc();
+            this.isConnected = false;
+        }
+
+        public bool IsConnected
+        {
+            get { return this.isConnected; }
+            private set { }
+        }
+
+        public bool Connect()
+        {
+            int dwFSReq=0;
+            bool result;
+            fsuipcClient.FSUIPC_Initialization();
+            result = fsuipcClient.FSUIPC_Open(dwFSReq, ref dwResult);
+
+            if(!result)
+                PrintErrorCode(dwResult);
+
+            return result;
+        }
+
+        public float[] GetTanksCapacity()
+        {
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            // Unit: Kg x 1000
+
+            float[] tanks = new float[7];
+            int i = 0;
+            float fuelDensity; // pounds per US gallon
+
+            // Fuel weight as pounds per gallon
+            result = this.fsuipcClient.FSUIPC_Read(0x0AF4, 4, ref token, ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+            if(result)
+                fuelDensity = dwResult / 256.0f;
+            else
+            {
+                fuelDensity = 0;
+                PrintErrorCode(dwResult);
+            }
+
+            // Tanks capacity in US Gallons
+            foreach (Int32 adr in AddrTabTotalFuelCapacity)
+            {
+                result = this.fsuipcClient.FSUIPC_Read(adr, 4, ref token, ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+                if (result)
+                {
+                    // Convert from US Gallons to Pounds then to Kg then to 1000xKg
+                    // 1000 x Kg = [gal] * [lbs/gal] * [kg/lbs] / []
+                    tanks[i] = dwResult * fuelDensity * 0.453592f / 1000.0f;
+                }
+                else
+                {
+                    tanks[i] = 0;
+                    PrintErrorCode(dwResult);
+                }
+                
+                i++;
+            }
+
+            return tanks;
+        }
+
+        public float[] GetTanksCurrentLevel()
+        {
+            // centre ; left main ; left aux ; left tip ; right main ; right aux ; right tip
+            // Unit: percentage
+
+            float[] tanks = new float[7];
+            int i = 0;
+
+            foreach (Int32 adr in AddrTabCurrentFuelLevels)
+            {
+
+                result = this.fsuipcClient.FSUIPC_Read(adr, 4, ref token, ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+                result &= this.fsuipcClient.FSUIPC_Get(ref token, ref dwResult);
+
+                if(result)
+                {
+                    tanks[i] = dwResult / 128.0f / 65536.0f;
+                    //tanks.Add(dwResult / 128.0f / 65536.0f);
+                }
+                else
+                {
+                    tanks[i] = 0;
+                    //tanks.Add(0);
+                    PrintErrorCode(dwResult);
+                }
+                i++;
+            }
+
+            return tanks;
+        }
+
+        public float GetTotalFuelCapacity()
+        {
+            // Unit: Kg x 1000
+
+            float[] capacities = GetTanksCapacity();
+            float total = 0;
+
+            foreach (float capacity in capacities)
+                total = total + capacity;
+
+            return total;
+        }
+
+        public float GetTotalFuelLevel()
+        {
+            float[] capacities = GetTanksCapacity();
+            float[] levels = GetTanksCurrentLevel();
+
+            float current = 0;
+            for(int i=0; i<7; i++)
+                current = current + (capacities[i] * levels[i]);
+
+            return current;
+        }
+
+        public double GetCurrentLoadedWeight()
+        {
+            // Unit: lbs
+            double loaded;
+
+            // Current loaded weight in lbs.
+            result = this.fsuipcClient.FSUIPC_Read(0x30C0, 8, ref token, ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Process(ref dwResult);
+            result &= this.fsuipcClient.FSUIPC_Get(ref token, 8, ref buffer);
+
+            if (result)
+            {
+                // Conversion du tableau de bytes en double (Float64)
+                loaded = BitConverter.ToDouble(buffer, 0);
+            }
+            else
+            {
+                loaded = 0;
+                PrintErrorCode(dwResult);
+            }
+
+            return loaded;
+        }
+
+        public void SetNewTankLevel(int tank_id, float level)
+        {
+            level = level * 128 * 65536;
+                
+            result = fsuipcClient.FSUIPC_Write(AddrTabCurrentFuelLevels[tank_id], (int)level, ref token, ref dwResult);
+            result &= fsuipcClient.FSUIPC_Process(ref dwResult);
+
+            if(!result)
+                PrintErrorCode(dwResult);
+        }
+
+
+        public static void PrintErrorCode(int code)
+        {
+            Console.WriteLine(translateResultCode(code));
+        }
+
+        public static string translateResultCode(int code) 
+        {
+            switch (code) 
+            {
+
+                case Fsuipc.FSUIPC_ERR_OK:
+                    return code + " = FSUIPC_ERR_OK";
+
+                case Fsuipc.FSUIPC_ERR_OPEN:
+                    return code + " = FSUIPC_ERR_OPEN";
+
+                case Fsuipc.FSUIPC_ERR_NOFS:
+                    return code + " = FSUIPC_ERR_NOFS";
+
+                case Fsuipc.FSUIPC_ERR_REGMSG:
+                    return code + " = FSUIPC_ERR_REGMSG";
+
+                case Fsuipc.FSUIPC_ERR_ATOM:
+                    return code + " = FSUIPC_ERR_ATOM";
+
+                case Fsuipc.FSUIPC_ERR_MAP:
+                    return code + " = FSUIPC_ERR_MAP";
+
+                case Fsuipc.FSUIPC_ERR_VIEW:
+                    return code + " = FSUIPC_ERR_VIEW";
+
+                case Fsuipc.FSUIPC_ERR_VERSION:
+                    return code + " = FSUIPC_ERR_VERSION";
+
+                case Fsuipc.FSUIPC_ERR_WRONGFS:
+                    return code + " = FSUIPC_ERR_WRONGFS";
+
+                case Fsuipc.FSUIPC_ERR_NOTOPEN:
+                    return code + " = FSUIPC_ERR_NOTOPEN";
+
+                case Fsuipc.FSUIPC_ERR_NODATA:
+                    return code + " = FSUIPC_ERR_NODATA";
+
+                case Fsuipc.FSUIPC_ERR_TIMEOUT:
+                    return code + " = FSUIPC_ERR_TIMEOUT";
+
+                case Fsuipc.FSUIPC_ERR_SENDMSG:
+                    return code + " = FSUIPC_ERR_SENDMSG";
+
+                case Fsuipc.FSUIPC_ERR_DATA:
+                    return code + " = FSUIPC_ERR_DATA";
+
+                case Fsuipc.FSUIPC_ERR_RUNNING:
+                    return code + " = FSUIPC_ERR_RUNNING";
+
+                case Fsuipc.FSUIPC_ERR_SIZE:
+                    return code + " = FSUIPC_ERR_SIZE";
+
+                case Fsuipc.FSUIPC_ERR_BUFOVERFLOW:
+                    return code + " = FSUIPC_ERR_BUFOVERFLOW";
+
+                default:
+                    return code.ToString();
+            }
+        }
+    }
+}
